6ebf1dfefc0ea465a99e6caf2806963f95d68123
songjiayu <1290257583@qq.com>
2017-03-19 19:24:11 -0700

    divert

--- winscard.c
+++ winscard.c
@@ -18,11 +18,20 @@
 
 #include "config.h"
 #include <stdarg.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
 #include "windef.h"
 #include "winbase.h"
 #include "wine/debug.h"
 #include "winscard.h"
 #include "winternl.h"
+
+
+#define IN_WINE_SC
+
 #include "winscard_impl.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(winscard);
@@ -54,6 +63,64 @@ BOOL WINAPI DllMain (HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
     return TRUE;
 }
 
+VOID WINAPI MyFree(LPVOID lpMem)
+{
+    HeapFree(GetProcessHeap(), 0, lpMem);
+}
+
+LPVOID WINAPI MyMalloc(DWORD dwSize)
+{
+    return HeapAlloc(GetProcessHeap(), 0, dwSize);
+}
+
+LPWSTR WINAPI MultiByteToUnicode(LPCSTR lpMultiByteStr, UINT uCodePage)
+{
+    LPWSTR lpUnicodeStr;
+    int nLength;
+
+    nLength = MultiByteToWideChar(uCodePage, 0, lpMultiByteStr,
+                                  -1, NULL, 0);
+    if (nLength == 0)
+        return NULL;
+
+    lpUnicodeStr = MyMalloc(nLength * sizeof(WCHAR));
+    if (lpUnicodeStr == NULL)
+        return NULL;
+
+    if (!MultiByteToWideChar(uCodePage, 0, lpMultiByteStr,
+                             nLength, lpUnicodeStr, nLength))
+    {
+        MyFree(lpUnicodeStr);
+        return NULL;
+    }
+
+    return lpUnicodeStr;
+}
+
+LPSTR WINAPI UnicodeToMultiByte(LPCWSTR lpUnicodeStr, UINT uCodePage)
+{
+    LPSTR lpMultiByteStr;
+    int nLength;
+
+    nLength = WideCharToMultiByte(uCodePage, 0, lpUnicodeStr, -1,
+                                  NULL, 0, NULL, NULL);
+    if (nLength == 0)
+        return NULL;
+
+    lpMultiByteStr = MyMalloc(nLength);
+    if (lpMultiByteStr == NULL)
+        return NULL;
+
+    if (!WideCharToMultiByte(uCodePage, 0, lpUnicodeStr, -1,
+                             lpMultiByteStr, nLength, NULL, NULL))
+    {
+        MyFree(lpMultiByteStr);
+        return NULL;
+    }
+
+    return lpMultiByteStr;
+}
+
 HANDLE WINAPI SCardAccessStartedEvent(void)
 {
     return g_startedEvent;
@@ -83,49 +150,58 @@ LONG WINAPI SCardAddReaderToGroupW(SCARDCONTEXT context, LPCWSTR reader, LPCWSTR
     return SCARD_S_SUCCESS;
 }
 
-LONG WINAPI SCardEstablishContext(DWORD dwScope, LPCVOID pvReserved1,
+/*LONG WINAPI SCardEstablishContext(DWORD dwScope, LPCVOID pvReserved1,
     LPCVOID pvReserved2, LPSCARDCONTEXT phContext)
 {
-    //FIXME("(%x,%p,%p,%p) stub\n", dwScope, pvReserved1, pvReserved2, phContext);
-    //SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-    //return SCARD_F_INTERNAL_ERROR;
-    return wine_SCardEstablishContext(dwScope,  pvReserved1,
+
+	return wine_SCardEstablishContext(dwScope,  pvReserved1,
      pvReserved2,  phContext);
-}
+}*/
 
-LONG WINAPI SCardIsValidContext(SCARDCONTEXT context)
+/*LONG WINAPI SCardIsValidContext(SCARDCONTEXT context)
 {
-    FIXME("(%lx) stub\n", context);
-    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-    return SCARD_F_INTERNAL_ERROR;
-}
+    return wine_SCardIsValidContext(context);
+}*/
 
-LONG WINAPI SCardListCardsA(SCARDCONTEXT hContext, LPCBYTE pbAtr, LPCGUID rgguidInterfaces, DWORD cguidInterfaceCount, LPSTR mszCards, LPDWORD pcchCards)
+LONG WINAPI SCardListCardsA(SCARDCONTEXT hContext, LPCBYTE pbAtr, LPCGUID rgguidInterfaces, 
+	DWORD cguidInterfaceCount, LPSTR mszCards, LPDWORD pcchCards)
 {
     FIXME(": stub\n");
     SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
     return SCARD_F_INTERNAL_ERROR;
 }
 
-LONG WINAPI SCardReleaseContext(SCARDCONTEXT context)
+LONG WINAPI SCardListCardsW(SCARDCONTEXT hContext, LPCBYTE pbAtr, LPCGUID rgguidInterfaces,
+	DWORD cguidInterfaceCount,LPWSTR mszCardsw,LPDWORD pcchCards)
 {
-    FIXME("(%lx) stub\n", context);
+    FIXME(": stub\n");
     SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
     return SCARD_F_INTERNAL_ERROR;
 }
 
-LONG WINAPI SCardStatusA(SCARDHANDLE context, LPSTR szReaderName, LPDWORD pcchReaderLen, LPDWORD pdwState, LPDWORD pdwProtocol, LPBYTE pbAtr, LPDWORD pcbAtrLen)
+/*LONG WINAPI SCardReleaseContext(SCARDCONTEXT context)
 {
-    FIXME("(%lx) stub\n", context);
-    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-    return SCARD_F_INTERNAL_ERROR;
+    return wine_SCardReleaseContext(context);
+}*/
+
+LONG WINAPI SCardStatusA(SCARDHANDLE context, LPSTR szReaderName, LPDWORD pcchReaderLen,
+	LPDWORD pdwState, LPDWORD pdwProtocol, LPBYTE pbAtr, LPDWORD pcbAtrLen)
+{
+   return wine_SCardStatus(context, szReaderName, pcchReaderLen,
+	 pdwState, pdwProtocol, pbAtr, pcbAtrLen);
 }
 
-LONG WINAPI SCardStatusW(SCARDHANDLE context, LPWSTR szReaderName, LPDWORD pcchReaderLen, LPDWORD pdwState,LPDWORD pdwProtocol,LPBYTE pbAtr,LPDWORD pcbArtLen)
+LONG WINAPI SCardStatusW(SCARDHANDLE context, LPWSTR szReaderName, 
+	LPDWORD pcchReaderLen, LPDWORD pdwState,LPDWORD pdwProtocol,LPBYTE pbAtr,LPDWORD pcbAtrLen)
 {
-    FIXME("(%lx) stub\n", context);
-    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-    return SCARD_F_INTERNAL_ERROR;
+    LONG ret;
+    LPSTR szReaderNameA;
+    szReaderNameA = UnicodeToMultiByte(szReaderName,CP_ACP);
+	
+	ret = SCardStatusA( context, szReaderNameA, pcchReaderLen, pdwState, pdwProtocol, pbAtr, pcbAtrLen);
+
+    szReaderName = MultiByteToUnicode(szReaderNameA,CP_ACP);
+	return ret;
 }
 
 void WINAPI SCardReleaseStartedEvent(void)
@@ -135,12 +211,149 @@ void WINAPI SCardReleaseStartedEvent(void)
 
 LONG WINAPI SCardListReadersA(SCARDCONTEXT context, const CHAR *groups, CHAR *readers, DWORD *buflen)
 {
-    FIXME("(%lx, %s, %p, %p) stub\n", context, debugstr_a(groups), readers, buflen);
-    return SCARD_E_NO_READERS_AVAILABLE;
+    fprintf(stderr,"SCardListReadersA \n");
+    LONG ret;
+    ret = wine_SCardListReaders(context, groups, readers, buflen);
+	fprintf(stderr,"readersA:%s \n", readers);
+	return ret;
 }
 
 LONG WINAPI SCardListReadersW(SCARDCONTEXT context, const WCHAR *groups, WCHAR *readers, DWORD *buflen)
 {
-    FIXME("(%lx, %s, %p, %p) stub\n", context, debugstr_w(groups), readers, buflen);
-    return SCARD_E_NO_READERS_AVAILABLE;
+    fprintf(stderr,"SCardListReadersW \n");
+    LONG ret;
+
+	CHAR *groupsA = NULL, *readersA = NULL;
+    groupsA = UnicodeToMultiByte(groups, CP_ACP);
+	readersA = UnicodeToMultiByte(readers, CP_ACP);
+    ret = SCardListReadersA( context,groupsA, readersA, buflen);
+
+	/*change out pama to W*/
+	readers = MultiByteToUnicode(readersA ,CP_ACP);
+	fprintf(stderr,"readersW:%s \n", readers);
+
+	return ret;
+}
+
+LONG WINAPI SCardConnectA(SCARDCONTEXT hContext,LPCSTR szReader,DWORD dwShareMode,DWORD dwPreferredProtocols,
+				LPSCARDHANDLE phCard,LPDWORD pdwActiveProtocol)
+{
+    return wine_SCardConnect( hContext, szReader, dwShareMode, dwPreferredProtocols,
+				 phCard, pdwActiveProtocol);
+}
+
+LONG WINAPI SCardConnectW(SCARDCONTEXT hContext,LPCWSTR szReaderW, DWORD dwShareMode, DWORD dwPreferredProtocols,
+	LPSCARDHANDLE phCard, LPDWORD pdwActiveProtocol)
+{
+	LPCSTR szReaderA;
+    szReaderA = UnicodeToMultiByte(szReaderW,CP_ACP);
+
+    return SCardConnectA(hContext, szReaderA, dwShareMode, dwPreferredProtocols, phCard, pdwActiveProtocol);
+}
+
+LONG WINAPI SCardControl(SCARDHANDLE hCard,DWORD dwControlCode,LPCVOID pbSendBuffer,DWORD cbSendLength,
+	LPVOID pbRecvBuffer,DWORD cbRecvLength,LPDWORD lpBytesReturned)
+{
+
+    return wine_SCardControl( hCard, dwControlCode, pbSendBuffer, cbSendLength,pbRecvBuffer, cbRecvLength, lpBytesReturned);
+
+}
+
+LONG WINAPI SCardDisconnect(SCARDHANDLE context,DWORD dwDisposition)
+{
+    return wine_SCardDisconnect(context, dwDisposition);
+}
+
+LONG WINAPI SCardEndTransaction(SCARDHANDLE hCard,DWORD dwDisposition)
+{
+    return wine_SCardEndTransaction(hCard, dwDisposition);
+}
+
+/*LONG WINAPI SCardFreeMemory(SCARDCONTEXT hContext,LPCVOID pvMem)
+{
+    return wine_SCardFreeMemory(hContext,pvMem);
+}*/
+
+LONG WINAPI SCardGetAttrib(SCARDHANDLE hCard,DWORD dwAttrId,LPBYTE pbAttr,LPDWORD pcbAttrLen)
+{
+    return wine_SCardGetAttrib( hCard, dwAttrId, pbAttr, pcbAttrLen);
+}
+
+LONG WINAPI SCardGetStatusChangeA(SCARDCONTEXT hContext,DWORD dwTimeout,
+	LPSCARD_READERSTATEA p_rgReaderStates,DWORD cReaders)
+{
+    fprintf(stderr,"SCardGetStatusChangeA \n");
+    SCARD_READERSTATEA *rgReaderStates = p_rgReaderStates;
+    return wine_SCardGetStatusChange( hContext, dwTimeout, rgReaderStates, cReaders);
+}
+
+LONG WINAPI SCardGetStatusChangeW(SCARDCONTEXT hContext,DWORD dwTimeout,
+	LPSCARD_READERSTATEW rgReaderStates,DWORD cReaders)
+{
+    fprintf(stderr,"SCardGetStatusChangeW \n");
+    LONG ret;
+	
+	SCARD_READERSTATEA rgReaderStatesA[1];
+
+	rgReaderStatesA[0].pvUserData = rgReaderStates[0].pvUserData;
+	rgReaderStatesA[0].dwCurrentState = rgReaderStates[0].dwCurrentState;
+	rgReaderStatesA[0].dwEventState = rgReaderStates[0].dwEventState;
+	rgReaderStatesA[0].cbAtr = rgReaderStates[0].cbAtr;
+
+	rgReaderStatesA[1].pvUserData = rgReaderStates[1].pvUserData;
+	rgReaderStatesA[1].dwCurrentState = rgReaderStates[1].dwCurrentState;
+	rgReaderStatesA[1].dwEventState = rgReaderStates[1].dwEventState;
+	rgReaderStatesA[1].cbAtr = rgReaderStates[1].cbAtr;
+
+	rgReaderStatesA[0].szReader = UnicodeToMultiByte(rgReaderStates [0].szReader,CP_ACP);
+	rgReaderStatesA[1].szReader = UnicodeToMultiByte(rgReaderStates [1].szReader,CP_ACP);
+
+	strcpy(rgReaderStatesA[0].rgbAtr,rgReaderStates[0].rgbAtr);
+	strcpy(rgReaderStatesA[1].rgbAtr,rgReaderStates[1].rgbAtr);
+	
+	ret = SCardGetStatusChangeA(hContext,dwTimeout,rgReaderStatesA,cReaders);
+
+    /*change out pama to W*/   
+	rgReaderStates[1].szReader = MultiByteToUnicode(rgReaderStatesA[1].szReader, CP_ACP);
+	
+	fprintf(stderr,"SCardGetStatusChangeW end \n");
+	return ret;
+}
+
+LONG WINAPI SCardListReaderGroupsA(SCARDCONTEXT hContext,LPSTR mszGroups,LPDWORD pcchGroups)
+{
+    return wine_SCardListReaderGroups( hContext, mszGroups, pcchGroups);
+}
+
+LONG WINAPI SCardListReaderGroupsW(SCARDCONTEXT hContext,LPWSTR mszGroups,LPDWORD pcchGroups)
+{
+   LONG ret;
+   LPSTR mszGroupsA;
+   mszGroupsA = HeapAlloc(GetProcessHeap(),0 ,sizeof(char)*(*pcchGroups));
+   ret = SCardListReaderGroupsA(hContext, mszGroupsA, pcchGroups);
+
+   mszGroups = MultiByteToUnicode(mszGroupsA, CP_ACP);
+
+   return ret;
+}
+
+LONG WINAPI SCardReconnect(SCARDHANDLE hCard,DWORD dwShareMode,DWORD dwPreferredProtocols,DWORD dwInitialization,LPDWORD pdwActiveProtocol)
+{
+    return wine_SCardReconnect(hCard, dwShareMode, dwPreferredProtocols,
+		 dwInitialization, pdwActiveProtocol);
+}
+
+LONG WINAPI SCardSetAttrib(SCARDHANDLE hCard,DWORD dwAttrId,LPCBYTE pbAttr,DWORD cbAttrLen)
+{
+    return wine_SCardSetAttrib(hCard, dwAttrId, pbAttr, cbAttrLen);
+}
+
+
+LONG WINAPI SCardTransmit(SCARDHANDLE hCard, LPCSCARD_IO_REQUEST pioSendPci, LPCBYTE pbSendBuffer,
+	DWORD cbSendLength,LPSCARD_IO_REQUEST pioRecvPci,LPBYTE pbRecvBuffer,LPDWORD pcbRecvLength)
+{
+    SCARD_IO_REQUEST *ioSendPci = pioSendPci;
+	SCARD_IO_REQUEST *ioRecvPic = pioRecvPci;
+
+    return wine_SCardTransmit( hCard, ioSendPci, pbSendBuffer, cbSendLength,ioRecvPic, pbRecvBuffer, pcbRecvLength);
 }
--- winscard.spec
+++ winscard.spec
@@ -7,57 +7,57 @@
 @ stdcall SCardAddReaderToGroupW(long wstr wstr)
 @ stub SCardBeginTransaction
 @ stub SCardCancel
-@ stub SCardConnectA
-@ stub SCardConnectW
-@ stub SCardControl
-@ stub SCardDisconnect
-@ stub SCardEndTransaction
-@ stdcall SCardEstablishContext(long ptr ptr ptr)
+@ stdcall SCardConnectA(long ptr long long long long)
+@ stdcall SCardConnectW(long ptr long long long long)
+@ stdcall SCardControl(long long long long long long long)
+@ stdcall SCardDisconnect(long long)
+@ stdcall SCardEndTransaction(long long)
+@ stdcall SCardEstablishContext(long ptr ptr ptr) wine_SCardEstablishContext
 @ stub SCardForgetCardTypeA
 @ stub SCardForgetCardTypeW
 @ stub SCardForgetReaderA
 @ stub SCardForgetReaderGroupA
 @ stub SCardForgetReaderGroupW
 @ stub SCardForgetReaderW
-@ stub SCardFreeMemory
-@ stub SCardGetAttrib
+@ stdcall SCardFreeMemory(long long) wine_SCardFreeMemory
+@ stdcall SCardGetAttrib(long long long long)
 @ stub SCardGetCardTypeProviderNameA
 @ stub SCardGetCardTypeProviderNameW
 @ stub SCardGetProviderIdA
 @ stub SCardGetProviderIdW
-@ stub SCardGetStatusChangeA
-@ stub SCardGetStatusChangeW
+@ stdcall SCardGetStatusChangeA(long long long long)
+@ stdcall SCardGetStatusChangeW(long long long long)
 @ stub SCardIntroduceCardTypeA
 @ stub SCardIntroduceCardTypeW
 @ stub SCardIntroduceReaderA
 @ stub SCardIntroduceReaderGroupA
 @ stub SCardIntroduceReaderGroupW
 @ stub SCardIntroduceReaderW
-@ stdcall SCardIsValidContext(long)
+@ stdcall SCardIsValidContext(long) wine_SCardIsValidContext
 @ stdcall SCardListCardsA(long ptr ptr long str long)
 @ stub SCardListCardsW
 @ stub SCardListInterfacesA
 @ stub SCardListInterfacesW
-@ stub SCardListReaderGroupsA
-@ stub SCardListReaderGroupsW
+@ stdcall SCardListReaderGroupsA(long ptr long)
+@ stdcall SCardListReaderGroupsW(long ptr long)
 @ stdcall SCardListReadersA(long str ptr ptr)
 @ stdcall SCardListReadersW(long wstr ptr ptr)
 @ stub SCardLocateCardsA
 @ stub SCardLocateCardsByATRA
 @ stub SCardLocateCardsByATRW
 @ stub SCardLocateCardsW
-@ stub SCardReconnect
-@ stdcall SCardReleaseContext(long)
+@ stdcall SCardReconnect(long long long long long)
+@ stdcall SCardReleaseContext(long) wine_SCardReleaseContext
 @ stdcall SCardReleaseStartedEvent()
 @ stub SCardRemoveReaderFromGroupA
 @ stub SCardRemoveReaderFromGroupW
-@ stub SCardSetAttrib
+@ stdcall SCardSetAttrib(long long long long)
 @ stub SCardSetCardTypeProviderNameA
 @ stub SCardSetCardTypeProviderNameW
 @ stub SCardState
 @ stdcall SCardStatusA (long str long long long ptr long )
 @ stdcall SCardStatusW (long wstr long long long ptr long )
-@ stub SCardTransmit
+@ stdcall SCardTransmit(long long ptr long long ptr long)
 @ extern g_rgSCardRawPci
 @ extern g_rgSCardT0Pci
 @ extern g_rgSCardT1Pci
